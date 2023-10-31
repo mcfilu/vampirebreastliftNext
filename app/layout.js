0608df7f1faa89f70e9549a2dc44e08d8c33ecc2
mcfilu <filip.1olszewski@gmail.com>
2023-10-31 11:42:36 +0100

    change of the google tag manager, new value from g site" "

--- app/layout.js
+++ app/layout.js
@@ -30,7 +30,7 @@ export default function RootLayout({ children }) {
           <GoogleAnalytics/>
         </Suspense>
         <Suspense fallback={<></>}>
-          <GoogleTagManager id="GTM-MN4QV5Z4" />
+          <GoogleTagManager id="G-XXKZYD5CRB" />
         </Suspense>
         
         
