3d5cc8ce79fc0f249f907d50d942f2b97556f25c
mcfilu <filip.1olszewski@gmail.com>
2023-12-13 23:00:07 +0100

    changing the header and paragraphs in the breastlift benefits both mobile and dekstop

--- app/vampirebreastlift/BreastLiftBenefits.jsx
+++ app/vampirebreastlift/BreastLiftBenefits.jsx
@@ -11,8 +11,8 @@ const BreastLiftBenefits = () => {
             <Image objectFit="cover" objectPosition="center" fill="true" alt="" src={boobs10}></Image>
         </div>
         <div className={`hidden md:flex flex flex-col w-[55%] p-[2vh]  items-center`}>
-            <h2 className={`text-gold1 text-[5vh] text-center font-header`}>Vampire BreastLift Benefits</h2>
-            <p className={`text-white font-poppins text-[2.1vh] text-justify mt-[3vh] mb-[2vh] font-main`}>The aim of the Vampire Breast Lift is to lift the breasts; provide a fuller, more pronounced cleavage and provide a more youthful look by enhancing the chest area’s skin. According to client reports, benefits of the treatment include:</p>
+            <h2 className={`text-gold1 text-[5vh] text-center font-header`}>Vampire Breast Lift™ Benefits</h2>
+            <p className={`text-white font-poppins text-[2.1vh] text-justify mt-[3vh] mb-[2vh] font-main`}>The aim of the Vampire Breast Lift™ is to lift the breasts; provide a fuller, more pronounced cleavage and provide a more youthful look by enhancing the chest area’s skin. According to client reports, benefits of the treatment include:</p>
             <ul className={`text-white text-[2.1vh] font-poppins w-full items-left`}>
                 <li className={`flex items-center mt-[1vh] font-main`}><i  className={`text-gold1 mr-4 fa-solid fa-circle text-[1.6vh] `}></i>Shapelier, firmer breasts</li>
                 <li className={`flex items-center mt-[1vh] font-main`}><i  className={`text-gold1 mr-4 fa-solid fa-circle text-[1.6vh]`}></i>Achieve a fuller, more pronounced cleavage</li>
@@ -26,12 +26,12 @@ const BreastLiftBenefits = () => {
         
 
       <div className={`md:hidden flex flex-col items-center`}>
-      <h2 className={`text-gold1 text-[5vh] text-center font-header`}>Vampire BreastLift Benefits</h2>
+      <h2 className={`text-gold1 text-[5vh] text-center font-header`}>Vampire Breast Lift™ Benefits</h2>
       {/* <video src={boobs10} autoPlay loop muted className={` object-cover h-[100%] w-[100%]`}></video> */}
       <div className={`flex w-full h-[30vh] relative`}> 
         <Image src={boobs10} objectFit="cover" objectPosition="center" fill="true" alt=""></Image>
       </div>
-      <p className={`text-white font-poppins text-[2.1vh] text-left pl-[1vh] mt-[3vh] mb-[2vh] font-main`}>The aim of the Vampire Breast Lift is to lift the breasts; provide a fuller, more pronounced cleavage and provide a more youthful look by enhancing the chest area’s skin. According to client reports, benefits of the treatment include:</p>
+      <p className={`text-white font-poppins text-[2.1vh] text-left pl-[1vh] mt-[3vh] mb-[2vh] font-main`}>The aim of the Vampire Breast Lift™ is to lift the breasts; provide a fuller, more pronounced cleavage and provide a more youthful look by enhancing the chest area’s skin. According to client reports, benefits of the treatment include:</p>
             <ul className={`text-white text-[2.1vh] font-poppins pl-[1vh]`}>
                 <li className={`flex items-center mt-[1vh] font-main`}><i  className={`text-gold1 mr-4 fa-solid fa-circle text-[1.6vh] `}></i>Shapelier, firmer breasts</li>
                 <li className={`flex items-center mt-[1vh] font-main`}><i  className={`text-gold1 mr-4 fa-solid fa-circle text-[1.6vh] `}></i>Achieve a fuller, more pronounced cleavage</li>
